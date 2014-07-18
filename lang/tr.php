5c265b0bb4f6d736eebee7f2722c0f0942c41fe4
hddntha <hddntha@users.noreply.github.com>
2014-07-18 14:15:12 +0300

    Create tr.php
    Language packet for Turkish.

---
+++ lang/tr.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Language: Türkçe / Turkish
+File: lang/tr.php
+Çeviri: Hayrettin Demirel - hddntha@hotmail.com
+*/ 
+
+
+$lang = array(
+    "decimal seperator"             => ".",
+    "thousands separator"           => ",",
+    "datetime format"               => "m/d/Y H:i", // www.php.net/manual/en/function.date.php
+    "Insert"                        => "Ekle",
+    "File"                          => "Dosya",
+    "Root"                          => "Ana Dizin",
+    "Close"                         => "Kapat",
+    "New folder"                    => "Yeni Klasör",
+    "Upload"                        => "Yükle",
+    "Clipboard"                     => "Pano",
+    "items"                         => "dosyalar",
+    "Change view"                   => "Görünümü Değiştir",
+    "View"                          => "Görünüm",
+    "Large images"                  => "Büyük Resimler",
+    "Small images"                  => "Küçük Resimler",
+    "List"                          => "Liste",
+    "Details"                       => "Detaylar",
+    "Tiles"                         => "Döşeme",
+    "Content"                       => "İçerik",
+    "Show the preview pane"         => "Önizleme panelini göster",
+    "Help"                          => "Yardım",
+    "Search"                        => "Ara",
+    "All files"                     => "Bütün Dosyalar",
+    "Dimensions"                    => "Ölçüler",
+    "Filename"                      => "Ad",
+    "Filetype"                      => "Tip",
+    "Size"                          => "Boyut",
+    "Modified on"                   => "Düzenlenme Tarihi",
+    "Directory"                     => "Klasör",
+    "Add a new folder"              => "Yeni Klasör Ekle",
+    "New folder is created in"      => "Yeni klasör şuraya oluşturulacak",
+    "Name of the new folder"        => "Yeni klasörün adı",
+    "Create folder"                 => "Yeni klasör ekle",
+    "Upload a new file"             => "Yeni dosya yükle",
+    "Image editor"                  => "Resim editörü",
+    "Browse..."                     => "Gözat...",
+    "Upload queue"                  => "Yükleme sürüyor",
+    "Currently uploading in folder" => "Klasöre yükleniyor",
+    "Select your file"              => "Dosyalarınızı seçin",
+
+    //Context menu
+    "Edit"                          => "Resmi düzenle",
+    "Copy"                          => "Kopyala",
+    "Cut"                           => "Kes",
+    "Paste"                         => "Yapıştır",
+    "Delete"                        => "Sil",
+    "Do you really want to delete this folder and its contents?" => "Bu klasörü ve içeriği silmek istediğinizden emin misiniz?",
+    "Do you really want to delete this file?"                    => "Bu dosyayı silmek istediğinizden emin misiniz?",
+    "Do you really want to delete this image?"                   => "Bu resmi silmek istediğinizden emin misiniz?",
+    "Open"                          => "Aç",
+    "Refresh"                       => "Yenile",
+    "Rename"                        => "Yeniden adlandır",
+
+    //Error, success and general messages
+    "The folder path was tampered with!"                         => "Klasör yolu hatalı görünüyor!",
+    "Creating new folder failed!"                                => "Klasör oluşturulamadı!<br />Yasak karakterler: ^ \\ / ? * \" ' &lt; &gt; : |",
+    "A new folder was created!"                                  => "Yeni bir klasör oluşturuldu",
+    "Creating the new folder failed!"                            => "Klasör oluşturulamadı!",  
+    "The files where successfully copied!"                       => "Dosyalar başarıyla kopyalandı!",
+    "The file or folder path was tampered with!"                 => "Klasör ve/veya dosya yolu hatalı görünüyor!",
+    "Deleting file failed!"                                      => "Dosya silinemedi!",
+    "Deleting folder failed!"                                    => "Klasör silinemedi!",
+    "%d file(s) successfully removed!"                           => "%d adlı dosya/lar silindi!",
+    "Select only one file to insert!"                            => "Lütfen eklemek için 1 dosya seçiniz!",
+    "Insert cancelled because there is no target to insert to!"  => "Hedef bulunamadığı için, ekleme işlemi yapılamadı!",
+    "Directory already exists!"                                  => "Bu isimle bir klasör var!",
+    "File already exists!"                                       => "Bu isimle bir dosya mevcut!",
+    "Name successfully changed!"                                 => "Dosya adı başarılı bir şekilde değiştirildi!",
+    "Rename failed!"                                             => "Dosya adı değiştirilemedi!",
+    "Please give a new name for file"                            => "Lütfen dosyaya yeni bir ad verin {0}{1}Dosya uzantıları otomatik eklenecektir!{1}(Yasaklı karakterler{2})",
+    "Please give a new name for folder"                          => "Lütfen dosyaya yeni bir ad verin {0}{1}(Yasaklı karakterler {2})",
+    "Action not allowed!"                                        => "Eylem gerçekleştirilemedi!",
+    "Invalid characters used!"                                   => "Yasaklı karakterler: ^ \\\ / ? * \\\" ' &lt; &gt; : | .",
+
+    //Upload
+    "Upload limited to %d MB!"      => "Azami Yükleme Boyutu: %d MB!",
+    "bytes"                         => "bytes",
+    "kB"                            => "kB",
+    "MB"                            => "MB",
+    "Cancel all uploads"            => "Bütün yüklemeleri iptal et",
+
+    //Settings
+    "Settings"                      => "Ayarlar",
+    "Language"                      => "Dil",
+    "Theme"                         => "Tema",
+    "Cookies need to be enabled to save your settings!"          => "Ayarları kaydetmemiz için çerezlere izin vermeniz gerekmektedir!",
+    "Save settings"                 => "Ayarları Kaydet",
+    "Settings saved!"               => "Ayarlar başarıyla kaydedildi!",
+	
+	"Absolute URL with hostname"    => "Web Url'si ile birlikte tam dosya yolu",
+
+    "EOF" => TRUE
+);
+
+?>
